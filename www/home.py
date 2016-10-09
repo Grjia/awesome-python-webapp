1bae92257b8863dd326786634bd1132917bbb060
Grjia <313228489@qq.com>
2016-10-09 10:29:33 +0800

    第二版

--- www/home.py
+++ www/home.py
@@ -74,3 +74,93 @@ def rate_sort(dict1):
     mysql.close()
     result_json = json.dumps(dic,cls=CJsonEncoder)
     return result_json
+
+#活跃用户数（单个apn）
+def active_query(dict1):
+    dict_tb = {'15':'wl_c_active_make_i15','60':'wl_c_active_make_h1','1440':'wl_c_active_make_d1'}
+    dic = {}
+    mysql = Mysql()
+    mysql.query('select rb_name from sys_region_base where rb_id = %s' % dict1['region_id'])
+    dic['city'] = mysql.result()
+    dic['status'] = '0'
+    dic['fields'] = '结束时间,活跃用户数'
+    if 'time_start' in dict1.keys():
+        mysql.query('select time_end,active_total from %s \
+        where  region_id = %s and apn = "%s" \
+        and time_end >= "%s" and time_end <= "%s"' \
+        % (dict_tb.get(dict1['type']),dict1['region_id'],dict1['apn'],dict1['time_start'],dict1['time_end']))
+    else:
+        mysql.query('select time_end,active_total from %s \
+        where region_id = %s and apn = "%s" \
+        and time_end = "%s"' % (dict_tb.get(dict1['type']),dict1['region_id'],dict1['apn'],dict1['time_end']))
+    dic['data'] = mysql.result()
+    mysql.close()
+    result_json = json.dumps(dic,cls=CJsonEncoder)
+    return result_json
+
+#活跃用户数（全省激活率前几或后几apn）
+def active_sort(dict1):
+    dict_tb = {'15':'wl_c_active_make_i15','60':'wl_c_active_make_h1','1440':'wl_c_active_make_d1'}
+    dic = {}
+    mysql = Mysql()
+    mysql.query('select rb_name from sys_region_base where rb_id = %s' % dict1['region_id'])
+    dic['city'] = mysql.result()
+    dic['status'] = '0'
+    dic['fields'] = '结束时间,apn,活跃用户数'
+    if dict1['num'] == '0':
+        mysql.query('select time_end,apn,active_total from %s \
+        where region_id = %s and time_end = "%s" order by active_total %s'\
+         % (dict_tb.get(dict1['type']),dict1['region_id'],dict1['time_end'],dict1['sort']))
+    else:
+        mysql.query('select time_end,apn,active_total from %s \
+        where region_id = %s and time_end = "%s" order by active_total %s limit %s'\
+         % (dict_tb.get(dict1['type']),dict1['region_id'],dict1['time_end'],dict1['sort'],dict1['num']))
+    dic['data'] = mysql.result()
+    mysql.close()
+    result_json = json.dumps(dic,cls=CJsonEncoder)
+    return result_json
+
+#流量数（单个apn）
+def traffic_query(dict1):
+    dict_tb = {'15':'wl_c_traffic_make_i15','60':'wl_c_traffic_make_h1','1440':'wl_c_traffic_make_d1'}
+    dic = {}
+    mysql = Mysql()
+    mysql.query('select rb_name from sys_region_base where rb_id = %s' % dict1['region_id'])
+    dic['city'] = mysql.result()
+    dic['status'] = '0'
+    dic['fields'] = '结束时间,上行流量,下行流量,总流量'
+    if 'time_start' in dict1.keys():
+        mysql.query('select time_end,sum_ul,sum_dl,sum_total from %s \
+        where  region_id = %s and apn = "%s" \
+        and time_end >= "%s" and time_end <= "%s"' \
+        % (dict_tb.get(dict1['type']),dict1['region_id'],dict1['apn'],dict1['time_start'],dict1['time_end']))
+    else:
+        mysql.query('select time_end,sum_ul,sum_dl,sum_total from %s \
+        where region_id = %s and apn = "%s" \
+        and time_end = "%s"' % (dict_tb.get(dict1['type']),dict1['region_id'],dict1['apn'],dict1['time_end']))
+    dic['data'] = mysql.result()
+    mysql.close()
+    result_json = json.dumps(dic,cls=CJsonEncoder)
+    return result_json
+
+#流量数（全省激活率前几或后几apn）
+def traffic_sort(dict1):
+    dict_tb = {'15':'wl_c_traffic_make_i15','60':'wl_c_traffic_make_h1','1440':'wl_c_traffic_make_d1'}
+    dic = {}
+    mysql = Mysql()
+    mysql.query('select rb_name from sys_region_base where rb_id = %s' % dict1['region_id'])
+    dic['city'] = mysql.result()
+    dic['status'] = '0'
+    dic['fields'] = '结束时间,apn,上行流量,下行流量,总流量'
+    if dict1['num'] == '0':
+        mysql.query('select time_end,apn,sum_ul,sum_dl,sum_total from %s \
+        where region_id = %s and time_end = "%s" order by active_total %s'\
+         % (dict_tb.get(dict1['type']),dict1['region_id'],dict1['time_end'],dict1['sort']))
+    else:
+        mysql.query('select time_end,apn,sum_ul,sum_dl,sum_total from %s \
+        where region_id = %s and time_end = "%s" order by sum_total %s limit %s'\
+         % (dict_tb.get(dict1['type']),dict1['region_id'],dict1['time_end'],dict1['sort'],dict1['num']))
+    dic['data'] = mysql.result()
+    mysql.close()
+    result_json = json.dumps(dic,cls=CJsonEncoder)
+    return result_json
--- www/server.py
+++ www/server.py
@@ -29,7 +29,7 @@ def rate_a():
 			if 'time_start' in dict1.keys():
 				time_size = date_limit(dict1['type'],dict1['time_start'],dict1['time_end'])
 				if time_size > 0:
-					dict2['status'] = '3'
+					dict2['status'] = '2'
 					return json.dumps(dict2)
 				else:
 					return rate_query(dict1)
@@ -56,6 +56,82 @@ def rate_b():
 	else:
 		return 'Use the method POST please !!!'
 
+#活跃用户数（单个apn）
+@app.route('/active', methods=['GET', 'POST'])
+def active_a():
+	dict2 = {}
+	if request.method == 'POST':
+		row = request.get_data()
+		dict1 = json.loads(row)
+		if dict1['type'] in ['15','60','1440']:
+			if 'time_start' in dict1.keys():
+				time_size = date_limit(dict1['type'],dict1['time_start'],dict1['time_end'])
+				if time_size > 0:
+					dict2['status'] = '2'
+					return json.dumps(dict2)
+				else:
+					return active_query(dict1)
+			else:
+				return active_query(dict1)
+		else:
+			dict2['status'] = '2'
+			return json.dumps(dict2)
+	else:
+		return 'Use the method POST please !!!'
+
+#活跃用户数（全省激活率前几或后几apn）
+@app.route('/active/sort', methods=['GET', 'POST'])
+def active_b():
+	dict2 = {}
+	if request.method == 'POST':
+		row = request.get_data()
+		dict1 = json.loads(row)
+		if dict1['type'] in ['15','60','1440']:
+			return active_sort(dict1)
+		else:
+			dict2['status'] = '2'
+			return json.dumps(dict2)
+	else:
+		return 'Use the method POST please !!!'
+
+#流量数（单个apn）
+@app.route('/traffic', methods=['GET', 'POST'])
+def traffic_a():
+	dict2 = {}
+	if request.method == 'POST':
+		row = request.get_data()
+		dict1 = json.loads(row)
+		if dict1['type'] in ['15','60','1440']:
+			if 'time_start' in dict1.keys():
+				time_size = date_limit(dict1['type'],dict1['time_start'],dict1['time_end'])
+				if time_size > 0:
+					dict2['status'] = '2'
+					return json.dumps(dict2)
+				else:
+					return traffic_query(dict1)
+			else:
+				return traffic_query(dict1)
+		else:
+			dict2['status'] = '2'
+			return json.dumps(dict2)
+	else:
+		return 'Use the method POST please !!!'
+
+#流量数（全省激活率前几或后几apn）
+@app.route('/traffic/sort', methods=['GET', 'POST'])
+def traffic_b():
+	dict2 = {}
+	if request.method == 'POST':
+		row = request.get_data()
+		dict1 = json.loads(row)
+		if dict1['type'] in ['15','60','1440']:
+			return traffic_sort(dict1)
+		else:
+			dict2['status'] = '2'
+			return json.dumps(dict2)
+	else:
+		return 'Use the method POST please !!!'
+
 @app.errorhandler(404)
 def not_found(error):
 	dict2 = {}
@@ -64,4 +140,5 @@ def not_found(error):
 
 
 if __name__ == '__main__':
-    app.run(host='192.168.1.7',port=int('8999'))
+    app.run(host='192.168.1.6',port=int('8999'))
+
